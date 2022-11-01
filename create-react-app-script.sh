438b0c8195cb56c04b6d0663940ff5d283ec4f46
micha <micha.vardy@tufin.com>
2022-11-01 09:48:39 +0200

    changes to bash script

--- create-react-app-script.sh
+++ create-react-app-script.sh
@@ -13,6 +13,53 @@ Help()
 Simplify()
 {
     echo "creating simplified project"
+    #initializing project
+    echo "creating react project named $1";
+    mkdir ./$1
+    cd ./$1
+    npm init -y
+    # entry point
+    mkdir public
+    cp $REACT_SCRIPTED_HOME/README.md ./
+    cp $REACT_SCRIPTED_HOME/include/index_simplified.html ./public/index.html
+    cp $REACT_SCRIPTED_HOME/include/package.json ./
+    # install packages
+    npm install 
+    # Configuration
+    cp $REACT_SCRIPTED_HOME/include/.babelrc ./
+    cp $REACT_SCRIPTED_HOME/include/webpack.config.js ./
+    # source code hello world
+    mkdir ./src
+    cp $REACT_SCRIPTED_HOME/include/index_simplified.js ./src/index.js
+    # to start
+    echo "write npm start in project root folder to start server"
+    echo "happy hacking :)"
+}
+
+Build () 
+{
+    #initializing project
+    echo "creating react project named $1";
+    mkdir ./$1
+    cd ./$1
+    npm init -y
+    # entry point
+    mkdir public
+    cp $REACT_SCRIPTED_HOME/README.md ./
+    cp $REACT_SCRIPTED_HOME/include/index.html ./public
+    cp $REACT_SCRIPTED_HOME/include/package.json ./
+    # install packages
+    npm install 
+    # Configuration
+    cp $REACT_SCRIPTED_HOME/include/.babelrc ./
+    cp $REACT_SCRIPTED_HOME/include/webpack.config.js ./
+    # source code hello world
+    mkdir ./src
+    cp $REACT_SCRIPTED_HOME/include/App.jsx ./src
+    cp $REACT_SCRIPTED_HOME/include/index.js ./src
+    # to start
+    echo "write npm start in project root folder to start server"
+    echo "happy hacking :)"
 }
 
 ### handeling options
@@ -29,35 +76,9 @@ while getopts ":h" option; do
     esac
 done
 
-#initializing project
-echo "creating react project named $1";
-mkdir ./$1
-cd ./$1
-npm init -y
-# entry point
-mkdir public
+Build
 
-#echo "Home directory: $REACT_SCRIPTED_HOME"
-#read -p "Are you sure? " -n 1 -r
-#echo    # (optional) move to a new line
-#if [[ ! $REPLY =~ ^[Yy]$ ]]
-#then
-#    exit 1
-#fi
 
-cp $REACT_SCRIPTED_HOME/README.md ./
-cp $REACT_SCRIPTED_HOME/include/index.html ./public
-cp $REACT_SCRIPTED_HOME/include/package.json ./
-# install packages
-npm install 
-# Configuration
-cp $REACT_SCRIPTED_HOME/include/.babelrc ./
-cp $REACT_SCRIPTED_HOME/include/webpack.config.js ./
-# source code hello world
-mkdir ./src
-cp $REACT_SCRIPTED_HOME/include/App.jsx ./src
-cp $REACT_SCRIPTED_HOME/include/index.js ./src
-# to start
-echo "write npm start in project root folder to start server"
-echo "happy hacking :)"
+
+
 
